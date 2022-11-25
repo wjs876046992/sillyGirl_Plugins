84c2bd514ad226fe53b008a4ef49182cec6d1fd9
Yi <leven_yi@qq.com>
2022-11-25 14:15:19 +0000

    update

--- download/b21f595a-55fa-11ed-95a9-fc7c02eb3d87.js
+++ download/b21f595a-55fa-11ed-95a9-fc7c02eb3d87.js
@@ -2,9 +2,10 @@
 * @author https://t.me/sillyGirl_Plugin
 * @version v1.0.0
 * @create_at 2022-09-08 15:06:22
-* @description 提交饿了么变量至青龙面板
-* @title 饿了么提交
+* @description 饿了么提交与查询
+* @title 饿了么
 * @rule elm ?
+* @rule 饿了么
  * @public false
 */
 
@@ -27,45 +28,136 @@ const GroupWhiteList=[]
 
 const s = sender
 const ql=require("qinglong")
-
+const db=new Bucket("elm_bind")
 
 function main(){
-    if(s.getChatId() && GroupWhiteList.indexOf(s.getChatId())==-1){
-        console.log("非白名单群聊，禁止提交")
-        return
-    }
-
-    let ck=s.param(1)
-    if(ck.indexOf("SID")==-1 || ck.indexOf("cookie2")==-1 || ck.indexOf("USERID")==-1){
-        s.reply("ck有误或者不完整")
-        return
-    }
-    let uid=ck.match(/(?<=USERID=)\d+/)[0]
-
     let token=ql.Get_QL_Token(Host,CilentID,CilentSecrect)
     if(!token){
         s.reply("token获取失败")
         return
     }
     let envs=ql.Get_QL_Envs(Host,token)
-    if(envs){
+    if(!envs){
+        s.reply("青龙变量获取失败")
+        return
+    }
+
+    if(s.getContent()=="饿了么"){
+        let uid=function (){
+            let ids=db.keys()
+            for(let i=0;i<ids.length;i++){
+                if(db.get(ids[i])==s.getUserId())
+                    return ids[i]
+            }
+        }()
+        let find=false
+        for(let i=0;i<envs.length;i++){
+            if(envs[i].name==EnvName){
+                let eid=envs[i].value.match(/(?<=USERID=)\d+/)
+                if(eid && eid==uid){
+                    find=true
+                    let bean_info=Get_ElmBeans(envs[i].value)
+                    let user_info=Get_ElmUserInfo(envs[i].value)
+                    if(bean_info && user_info){
+                        let msg="账号："+user_info.username
+                        msg+="\n吃货豆总数："+bean_info.amount
+                        msg+="\n今日收入："+bean_info.increment
+                        s.reply(msg)
+                    }
+                    else{
+                        s.reply("查询失败")
+                    }
+                    break
+                }
+            }
+        }
+        if(!find)
+            s.reply("未绑定饿了么账号")
+    }
+    else{
+        if(s.getChatId() && GroupWhiteList.indexOf(s.getChatId())==-1){
+            console.log("非白名单群聊，禁止提交")
+            return
+        }
+
+        let ck=s.param(1)
+        if(ck.indexOf("SID")==-1 || ck.indexOf("cookie2")==-1 || ck.indexOf("USERID")==-1){
+            s.reply("ck有误或者不完整")
+            return
+        }
+        let uid=ck.match(/(?<=USERID=)\d+/)[0]
+        let find=false
         for(let i=0;i<envs.length;i++){
             if(envs[i].name==EnvName){
                 let eid=envs[i].value.match(/(?<=USERID=)\d+/)
                 if(eid && eid[0]==uid){
+                    find=true
                     if(envs[i].id)
                         env_id=envs[i].id
                     else
                         env_id=envs[i]._id
                     if(ql.Update_QL_Env(Host,token,env_id,envs[i].name,ck,envs[i].remarks)){
+                        db.set(uid,s.getUserId())
                         s.reply("更新成功")
                         return
                     }
                 }
             }
         }
+        if(!find){
+            if(ql.Add_QL_Env(Host,token,EnvName,ck,s.getPlatform()+":"+s.getUserId())){
+                db.set(uid,s.getUserId())
+                s.reply("添加成功")
+                return
+            }
+        }
+        s.reply("提交失败")
     }
-    s.reply("更新失败")
 }
 
+function Get_ElmBeans(ck){
+    let resp=request({
+        url:"https://h5.ele.me/restapi/svip_biz/v1/supervip/foodie/records?offset=0&limit=100",
+		method:"get",
+        headers:{
+            Cookie:ck
+        }
+    })
+	try{
+        let info=JSON.parse(resp.body)
+        let count=0
+		let day0=null
+		for(let i=0;i<info.records.length;i++){
+			let day=info.records[i].createdTime.match(/(?<=-)\d{1,2}(?= )/)[0]
+			if(!day0)
+				day0=day
+			if(day == day0)
+				count+=info.records[i].count
+			else
+				break
+		}
+    	return {
+                	amount:info.peaCount,
+                	increment:count
+        		}
+	}
+	catch(err){
+		return null
+	}
+}
+function Get_ElmUserInfo(ck){
+    let resp=request({
+        url:"https://restapi.ele.me/eus/v4/user_mini",
+		method:"get",
+        headers:{
+            Cookie:ck
+        }
+    })
+    try{
+        return JSON.parse(resp.body)
+    }
+    catch(err){
+        return null
+    }
+}
 main()
